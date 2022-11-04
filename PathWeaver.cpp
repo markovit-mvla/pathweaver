11b0b2689d47eed66c99f6669a574aacf0ff3395
Tohar Markovich <80293744+markovit-mvla@users.noreply.github.com>
2022-11-03 20:20:25 -0700

    Update PathWeaver.cpp

--- PathWeaver.cpp
+++ PathWeaver.cpp
@@ -9,7 +9,7 @@
 PathWeaver::PathWeaver()
 {
     /* Can change x to determine how much of an arc the path is */
-    double x = 0.0;
+    double x = 1.0;
     PATH_TYPE path_ = &purePursuit(SwerveDrive::getDistance(SwerveDrive::getRobotGoalAng()) / x);
     /* Add path to network table */
     frc::SmartDashboard::PutData("Path", path_);
